40a17017a82bb1c9be765419131eac85a401c38f
partylikeits1983 <aleqvids@gmail.com>
2025-02-14 16:54:55 -0500

    cleanup

--- src/bin/ephemeral_notes.rs
+++ src/bin/ephemeral_notes.rs
@@ -129,7 +129,7 @@ async fn main() -> Result<(), ClientError> {
     let mut seeds = vec![];
     let mut key_pairs = vec![];
 
-    let number_of_accounts = 2500;
+    let number_of_accounts = 50;
 
     for _ in 0..number_of_accounts {
         let init_seed = ChaCha20Rng::from_entropy().gen();
@@ -249,7 +249,7 @@ async fn main() -> Result<(), ClientError> {
     let mut landed_blocks = vec![];
 
     let start = Instant::now();
-    for i in 0..number_of_accounts-1 {
+    for i in 0..number_of_accounts - 1 {
         let loop_start = Instant::now();
 
         println!("\nephemeral tx {:?}", i + 1);
